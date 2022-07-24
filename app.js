ebcfce2bff47f62a84009ffa80f25cd8d10aa69c
Tricia <tricianghiem@gmail.com>
2022-07-24 14:13:58 -0500

    add functions to see tables

--- app.js
+++ app.js
@@ -12,49 +12,97 @@ const { allowedNodeEnvironmentFlags } = require("process");
 // });
 
 const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "name",
-      message: "what would you like to do?",
-      choices: [
-        "View all Employees",
-        "Add Employees",
-        "Add Employee",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Departments",
-        "Quit"],
-    }
-  ])
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "name",
+          message: "what would you like to do?",
+          choices: [
+            "View all Employees",
+            "Add Employees",
+            "Add Employee",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Departments",
+            "Quit",
+          ],
+        },
+      ])
 
-  // responses to answers 
-  .then((answers) => {
-    const { choices } = answers; 
+      // responses to answers
+      .then((answers) => {
+        const { choices } = answers;
 
-    if (choices === "View All Employees") {
-        showRoles();
-      }
+        if (choices === "View All Employees") {
+          viewEmployees();
+        }
 
-    if (choices === "Add Employee Role") { 
-        addEmployee();
-    }
+        if (choices === "Add Employee Role") {
+          addEmployee();
+        }
 
-    if (choices === "View All Roles") {
-        showRoles();
-    }
+        if (choices === "View All Roles") {
+          viewRoles();
+        }
 
-    if (choices === "Add Role") {
-        addRole();
-    }
+        if (choices === "Add Role") {
+          addRole();
+        }
 
-    if (choices === "View All Departments") {
-      showDepartments();
-    }
+        if (choices === "View All Departments") {
+          viewDepartments();
+        }
 
-    if (choices === "Add Departments") {
-        addDepartments();
-    };
-});
+        if (choices === "Add Departments") {
+          addDepartments();
+        }
+      })
+  );
+};
+
+//function to show all data on selected table
+viewDepartments = () => {
+  const sql = `SELECT department.id AS id, department.name AS department 
+    FROM department`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+viewRoles = () => {
+  const sql = `SELECT role.id, role.title, department.name AS department
+    FROM role
+    INNER JOIN department ON role.department_id = department.id`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+viewEmployees = () => {
+  const sql = `SELECT employee.id, 
+                        employee.first_name, 
+                        employee.last_name, 
+                        role.title, 
+                        department.name AS department,
+                        role.salary, 
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                 FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
 };
